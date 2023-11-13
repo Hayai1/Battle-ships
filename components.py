b86460a53f65e05d2bb183abc20e54f3283eda1c
Dylan <dylanpjhough@gmail.com>
2023-11-13 14:51:50 +0000

    components module added

--- components.py
+++ components.py
@@ -1,6 +1,6 @@
 
-import random
-
+from random import randint
+from json import load
 def initialise_board(size=10):
     board = []
     for i in range(size):
@@ -15,7 +15,7 @@ def create_battleships(filename="battleships.txt"):
     shipData = file.readlines()[0].split(",")
     file.close()
     for ship in range(0, len(shipData), 2):
-        ships[shipData[ship]] = shipData[ship+1]
+        ships[shipData[ship]] = int(shipData[ship+1])
     return ships
 
 
@@ -31,13 +31,44 @@ def print_board(board):
         print()
 
 def place_battleships(board, ships, configuration='simple'):
+    boardDimensions = (len(board)-1,len(board[0])-1)
     if (configuration == 'simple'):
         row = 0
         for ship in ships:
-            random.randint(len(board[0]))
-    if (configuration == 'random'):
-        pass
+            x = randint(0,boardDimensions[0]-ships[ship])
+            for walkCounter in range(ships[ship]):
+                board[row][x+walkCounter] = ship
+            row+=1
+    elif (configuration == 'random'):
+        for ship in ships:
+            shipLength = ships[ship]
+            findingViableLocationForTheShip = True
+            while findingViableLocationForTheShip:
+                x = randint(0,boardDimensions[0])
+                y = randint(0,boardDimensions[1])
+                directionMultiplier = ((0,1),(1,0),(0,-1),(-1,0))[randint(0,3)]
+                locs = []
+                for walkCounter in range(shipLength):
+                    loc = (x+walkCounter*directionMultiplier[0],y+walkCounter*directionMultiplier[1])
+                    if (loc[0] < 0 or loc[0]>boardDimensions[0] or 
+                        loc[1] < 0 or loc[1]>boardDimensions[1] or 
+                        board[loc[0]][loc[1]] != None):
+                        break
+                    locs.append(loc)
+                if len(locs) == shipLength:
+                    for loc in locs:
+                        board[loc[0]][loc[1]] = ship
+                    findingViableLocationForTheShip = False   
     else:
-        pass
+        placementDirections = { 'right':(1,0), 'left':(-1,0), 'up':(0,-1), 'down':(0,1) }
+        with open(configuration) as f:data = load(f)
+        shiplocations = data['shipLocs']
+        for ship in shiplocations:
+            shipLengths = ships[ship['name']]
+            placementDirection = placementDirections[ship['direction']]
+            for walkCounter in range(0, shipLengths):
+                board[ship['y']+walkCounter*placementDirection[1]][ship['x']+walkCounter*placementDirection[0]] = ship['name']
+    return board
 
-create_battleships()
+board = place_battleships(initialise_board(),create_battleships(),'placement.json')
+print_board(board)
